e4fcab91f2c5daa1768e39ceade91cc3866c6340
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-03-16 11:56:02 +0100

    tailRecursion foldLeft

--- src/main/scala/it/unibo/pps/u03/task2.scala
+++ src/main/scala/it/unibo/pps/u03/task2.scala
@@ -3,9 +3,12 @@ package it.unibo.pps.u03
 import u03.Sequences.*
 import Sequence.*
 
+import scala.annotation.tailrec
+
 object task2 extends App {
-  def foldLeft[A,B](s: Sequence[A])(default: B)(acc: (first: B, second: A) => B) : Sequence[B] = s match
-    case Nil() => Cons(default, Nil())
+  @tailrec
+  def foldLeft[A,B](s: Sequence[A])(default: B)(acc: (first: B, second: A) => B) : B = s match
+    case Nil() => default
     case Cons(h, t) => foldLeft(t)(acc.apply(default, h))(acc)
 
   val lst = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
