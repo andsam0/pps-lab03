1a5ab047a1dc2363f96be15ede9ed4fbb400883b
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-03-16 11:54:40 +0100

    fixed foldLeft

--- src/main/scala/it/unibo/pps/u03/task2.scala
+++ src/main/scala/it/unibo/pps/u03/task2.scala
@@ -5,8 +5,9 @@ import Sequence.*
 
 object task2 extends App {
   def foldLeft[A,B](s: Sequence[A])(default: B)(acc: (first: B, second: A) => B) : Sequence[B] = s match
-    case Nil() => Nil()
-    case Cons(h, t) => foldLeft(t)(acc.apply(default, h))(acc)
+    case Nil() => Cons(default, Nil())
+    case Cons(h, t) =>
+      foldLeft(t)(acc.apply(default, h))(acc)
 
   val lst = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
   println(foldLeft(lst)(0)(_ - _)) // -16
